a750555a990c577e5278734de341003c76e93f8c
Nikolay <freshitftm@gmail.com>
2021-05-20 11:49:09 +0300

    Add App without JSX

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
 
-function App() {
+/*function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -19,6 +20,39 @@ function App() {
         </a>
       </header>
     </div>
+  );
+}*/
+
+function App() {
+  return React.createElement(
+    "div",
+    { className: "App" },
+    React.createElement(
+      "header",
+      { className: "App-header" },
+      React.createElement("img", {
+        src: logo,
+        className: "App-logo",
+        alt: "logo",
+      }),
+      React.createElement(
+        "p",
+        null,
+        "Edit ",
+        React.createElement("code", null, "src/App.js"),
+        " and save to reload."
+      ),
+      React.createElement(
+        "a",
+        {
+          className: "App-link",
+          href: "https://reactjs.org",
+          target: "_blank",
+          rel: "noopener noreferrer",
+        },
+        "Learn React"
+      )
+    )
   );
 }
 
