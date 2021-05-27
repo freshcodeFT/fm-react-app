4096a5d513bdaec7c84c7f1d9599ac4b9bd23855
Nikolay <freshitftm@gmail.com>
2021-05-27 10:22:30 +0300

    Add classnames

---
+++ src/components/SignUpForm/FormInput/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import cx from 'classnames';
+import style from "./FormInput.module.css";
+
+export default function FormInput(props) {
+  const {value, onChange, isValid, placeholder, type = "text"} = props;
+  return (
+    <input
+      value={value}
+      onChange={onChange}
+      className={cx(
+        style.input,
+        { [style.invalidInput]: !isValid },
+        { [style.validInput]: isValid }
+      )}
+      type={type}
+      placeholder={placeholder}
+    />
+  );
+}
--- src/components/SignUpForm/index.jsx
+++ src/components/SignUpForm/index.jsx
@@ -1,38 +1,52 @@
 import React, { Component } from "react";
+import cx from "classnames";
 import style from "./SignUpForm.module.css";
 
 const intialValues = {
-  firstname: '',
-  lastname: '',
-  email: '',
-  password: '',
-}
+  firstname: "",
+  lastname: "",
+  email: "",
+  password: "",
+  isfirstnameValid: true,
+  islastnameValid: true,
+  isemailValid: true,
+  ispasswordValid: true,
+};
 
 class SignUpForm extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = {...intialValues}
+    this.state = { ...intialValues };
   }
 
   submitHandler = (event) => {
-    const {registerUser} = this.props;
+    const { registerUser } = this.props;
     event.preventDefault();
-    registerUser({...this.state});
-    this.setState({...intialValues});
+    registerUser({ ...this.state });
+    this.setState({ ...intialValues });
   };
 
-  handleChange = ({target: {name, value}}) => {
-    this.setState({[name]: value});
-  }
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({ [name]: value, [`is${name}Valid`]: !value.includes(" ") });
+  };
 
   render() {
-    const {firstname, lastname, email, password} = this.state;
+    const {
+      firstname,
+      lastname,
+      email,
+      password,
+      isemailValid,
+      isfirstnameValid,
+      islastnameValid,
+      ispasswordValid,
+    } = this.state;
     return (
       <form className={style.container} onSubmit={this.submitHandler}>
         <input
           value={firstname}
           onChange={this.handleChange}
-          className={style.input}
+          className={cx(style.input, {[style.invalidInput]: !isfirstnameValid}, {[style.validInput]: isfirstnameValid})}
           type="text"
           name="firstname"
           placeholder="Введите имя"
@@ -40,7 +54,7 @@ class SignUpForm extends Component {
         <input
           value={lastname}
           onChange={this.handleChange}
-          className={style.input}
+          className={cx(style.input, {[style.invalidInput]: !islastnameValid},  {[style.validInput]: islastnameValid})}
           type="text"
           name="lastname"
           placeholder="Введите фамилию"
@@ -48,7 +62,7 @@ class SignUpForm extends Component {
         <input
           value={email}
           onChange={this.handleChange}
-          className={style.input}
+          className={cx(style.input, {[style.invalidInput]: !isemailValid},  {[style.validInput]: isemailValid})}
           type="email"
           name="email"
           placeholder="Введите email"
@@ -56,7 +70,7 @@ class SignUpForm extends Component {
         <input
           value={password}
           onChange={this.handleChange}
-          className={style.input}
+          className={cx(style.input, {[style.invalidInput]: !ispasswordValid}, {[style.validInput]: ispasswordValid})}
           type="password"
           name="password"
           placeholder="Введите пароль"
