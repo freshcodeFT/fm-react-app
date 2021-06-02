d52a318838d377ccc5912858b76a81ce0415143f
Nikolay <freshitftm@gmail.com>
2021-06-02 10:12:03 +0300

    Remove unused imports

--- src/components/Calendar/index.jsx
+++ src/components/Calendar/index.jsx
@@ -1,24 +1,11 @@
 import React, { useState } from "react";
-import style from './Calendar.module.sass';
-import {
-  setDate,
-  endOfMonth,
-  getWeeksInMonth,
-  startOfWeek,
-  add,
-  eachWeekOfInterval,
-} from "date-fns";
+import style from "./Calendar.module.sass";
+import { setDate, endOfMonth, eachWeekOfInterval } from "date-fns";
 import Week from "./Week";
 import CalendarHeader from "./CalendarHeader";
 
 export default function Calendar() {
   const [currentDate, setCurrentDate] = useState(new Date());
-  /*const weeksInMonth = getWeeksInMonth(currentDate);
-  const startOfWeekDate = startOfWeek(currentDate);
-   const nextWeekDate = add(startOfWeekDate, {days: 1,})
-  const tomorrowDate = add(currentDate, {
-    days: 1,
-  })*/
   const weeksInMonthArray = eachWeekOfInterval({
     start: setDate(currentDate, 1),
     end: endOfMonth(currentDate),
