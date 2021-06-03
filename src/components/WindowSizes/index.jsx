aa7e0d0ff223ed43407b5d197111e940bd1adce1
Nikolay <freshitftm@gmail.com>
2021-06-03 11:54:29 +0300

    Add task solution

--- src/components/WindowSizes/index.jsx
+++ src/components/WindowSizes/index.jsx
@@ -1,6 +1,37 @@
-/* 
-<div>
-  Current width of viewport is: {1920}
-  Current height of viewport is: {450} 
-</div>
-*/
+import React, { Component } from 'react';
+
+export default class WindowSizes extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      currentWidth: 0,
+      currentHeight: 0,
+    };
+  }
+
+  handleResize = () => {
+    this.setState({
+      currentHeight: window.innerHeight,
+      currentWidth: window.innerWidth,
+    });
+  };
+
+  componentDidMount () {
+    this.handleResize();
+    window.addEventListener('resize', this.handleResize);
+  }
+
+  componentWillUnmount () {
+    window.removeEventListener('resize', this.handleResize);
+  }
+
+  render () {
+    const { currentHeight, currentWidth } = this.state;
+    return (
+      <div>
+        Current width of viewport is: {currentWidth}
+        Current height of viewport is: {currentHeight}
+      </div>
+    );
+  }
+}
