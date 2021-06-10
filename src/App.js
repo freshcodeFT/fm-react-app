5e3c05b9d1e63ef265c32dfee21c1de751afefd5
Nikolay <freshitftm@gmail.com>
2021-06-10 10:51:24 +0300

    Add task template

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import UserLoader from './components/UserLoader';
 import PhonesLoader from './components/PhonesLoader';
 import MouseTrackerPage from './pages/MouseTrackerPage';
 import Tree from './components/Tree';
+import Home from './pages/Home';
 import { UserContext, ThemeContext } from './contexts';
 
 import CONSTANTS from './constants';
@@ -49,8 +50,6 @@ function App (props) {
     </BrowserRouter>
   );
 }
-
-const Home = () => <div>HOME</div>;
 const About = props => {
   console.log('ABOUT: ', props);
 
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import style from './Home.module.sass'
+
+
+/*
+  1. Переделать переключение тем на хуки
+  2. Добавить переключение темы в Home
+*/
+function Home () {
+  return <div className={style.container}>HOME PAGE</div>;
+}
+
+export default Home;
