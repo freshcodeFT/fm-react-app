228d5ab7a93ff074ba4e3b40b8eeff207226967a
Nikolay <freshitftm@gmail.com>
2021-06-11 11:39:06 +0300

    Add task template

--- src/components/UserLoader/index.jsx
+++ src/components/UserLoader/index.jsx
@@ -1,30 +1,28 @@
 import React from 'react';
 import DataProvider from '../DataProvider';
 import Spinner from '../Spinner';
+import { useData } from '../../hooks';
 
 function UserLoader () {
   const load = () => {
     return fetch('/users.json').then(res => res.json());
   };
-  return (
-    <DataProvider loadData={load}>
-      {({ data, isFetching, error }) => {
-        if (!isFetching && data) {
-          return (
-            <div>
-              {data.map(({ id, name }) => (
-                <div key={id}>
-                  ID: {id}, NAME: {name}
-                </div>
-              ))}
-            </div>
-          );
-        }
-        if (error) return <div>ERROR</div>;
-        return <Spinner />;
-      }}
-    </DataProvider>
-  );
+
+  const { data, error, isFetching } = useData(load);
+
+  if (!isFetching && data) {
+    return (
+      <div>
+        {data.map(({ id, name }) => (
+          <div key={id}>
+            ID: {id}, NAME: {name}
+          </div>
+        ))}
+      </div>
+    );
+  }
+  if (error) return <div>ERROR</div>;
+  return <Spinner />;
 }
 
 export default UserLoader;
---
+++ src/hooks/index.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react';
+
+export function useData (loadData) {
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    setIsFetching(true);
+
+    loadData()
+      .then(data => {
+        setData(data);
+      })
+      .catch(err => setError(err))
+      .finally(() => setIsFetching(false));
+  }, []);
+  return { data, error, isFetching };
+}
+
+/*
+  Написать hook для отслеживания позиции курсора
+*/
+export function useMouse() {}
