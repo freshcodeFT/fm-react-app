7c68fb194690a5df1b450da3c3af016c9852995b
Nikolay <freshitftm@gmail.com>
2021-06-16 11:13:49 +0300

    Add Formik form example

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import style from './App.module.sass';
 import React, { useState } from 'react';
-import { SIGN_UP_SCHEMA } from 'utils/validationSchemas';
+import SignInForm from 'components/forms/SignInForm';
 
 function App (props) {
-  console.dir(SIGN_UP_SCHEMA);
-  const data = SIGN_UP_SCHEMA.validateSync({
-    password: 'Ab1@udhr9u2ucsherd',
-    email: 'test@gmail.com',
-    lastName: 'Test',
-    firstName: 'Name'
-  });
-  console.log(data);
-  return <></>;
+  return <SignInForm />;
 }
 
 export default App;
--- src/components/forms/SignInForm/index.jsx
+++ src/components/forms/SignInForm/index.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { SIGN_IN_SCHEMA } from 'utils/validationSchemas';
 
-function SignInForm() {
+const initialValues = {
+  email: '',
+  password: '',
+};
+
+function SignInForm () {
   return (
-    <div>
-      
-    </div>
-  )
+    <Formik initialValues={initialValues} validationSchema={SIGN_IN_SCHEMA}>
+      {formikProps => {
+        console.log(formikProps);
+        return (
+          <Form>
+            <Field name='email' />
+            <ErrorMessage name='email' component='span' />
+            <Field type='password' name='password' />
+            <ErrorMessage name='password' component='span' />
+            <Field type='submit' value='Submit' />
+          </Form>
+        );
+      }}
+    </Formik>
+  );
 }
 
 export default SignInForm;
